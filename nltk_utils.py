477caad6b2bc8e5f4c98d543c57d604dfd1aa31d
nourammar <109729864+ammarnour@users.noreply.github.com>
2023-05-05 15:34:27 +0100

    Update nltk_utils.py
    Signed-off-by: nourammar <109729864+ammarnour@users.noreply.github.com>

--- nltk_utils.py
+++ nltk_utils.py
@@ -41,3 +41,34 @@ def pos_tag(sentence):
     return pos_tags
 
 # Analyse syntaxique (Parsing)
+
+#
+def remove_stop_words(sentence):
+    stop_words = set(stopwords.words('french')) # ou une autre langue
+    words = word_tokenize(sentence)
+    clean_words = [word for word in words if word.lower() not in stop_words]
+    clean_sentence = " ".join(clean_words)
+    return clean_sentence
+ 
+def calculate_word_frequency(sentence):
+    # Tokeniser le sentencee en mots
+    words = nltk.word_tokenize(sentence.lower())
+
+    # Calculer la fréquence des mots
+    fdist = FreqDist(words)
+
+    # Retourner la distribution de fréquence des mots
+    return fdist
+
+def find_collocations(text):
+    # Tokeniser le texte en mots
+    words = nltk.word_tokenize(text.lower())
+
+    # Créer un objet de texte NLTK
+    text_obj = nltk.Text(words)
+
+    # Trouver les collocations
+    collocations = text_obj.collocations()
+
+    # Retourner les collocations
+    return collocations
